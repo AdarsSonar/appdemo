3a7f142282b1901a58cba87288600a628cd0ee64
Adars Sonar <adars.sonar@capgemini,com>
2025-02-06 12:07:21 +0530

    hello

--- src/main/java/com/telusko/quizapp/dao/QuestionDao.java
+++ src/main/java/com/telusko/quizapp/dao/QuestionDao.java
@@ -14,4 +14,5 @@ public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     @Query(value = "SELECT * FROM question q Where q.category=:category ORDER BY RANDOM() LIMIT :numQ", nativeQuery = true)
     List<Question> findRandomQuestionsByCategory(String category, int numQ);
+//    hello
 }
